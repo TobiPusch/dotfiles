23c0af80e3c25d66f7071758d1456c6f12394002
tobi <puschtobi@gmail.com>
2026-08-04 21:40:13 +0200

    clangd performance fix

--- NeoVim/.config/nvim/lua/config/plugins/mason.lua
+++ NeoVim/.config/nvim/lua/config/plugins/mason.lua
@@ -1,52 +1,57 @@
 return {
-    -- Mason zum Verwalten von LSP-Servern, Lintern und Formattern
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	-- Mason zum Verwalten von LSP-Servern, Lintern und Formattern
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    -- Brücke zwischen Mason und lspconfig
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "clangd", "rust_analyzer" },
-            })
-        end,
-    },
+	-- Brücke zwischen Mason und lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "clangd", "rust_analyzer" },
+			})
+		end,
+	},
 
-    -- Die eigentlichen LSP-Konfigurationen
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { 
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp", -- Wichtig für die Autocomplete-Fähigkeiten
-        },
-        config = function()
-            -- Lade die Fähigkeiten deines Autocomplete-Plugins
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	-- Die eigentlichen LSP-Konfigurationen
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- Wichtig für die Autocomplete-Fähigkeiten
+		},
+		config = function()
+			-- Lade die Fähigkeiten deines Autocomplete-Plugins
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- C/C++ Setup mit der NEUEN nativen API
-            vim.lsp.config("clangd", {
-                cmd = {
-                    "clangd",
-                    "--background-index",
-                    "--header-insertion=never",
-                    "--query-driver=/usr/bin/gcc,/usr/bin/g++", -- Fix für Arch Linux
-                },
-                filetypes = { "c", "cpp", "objc", "objcpp" },
-                capabilities = capabilities, -- <-- HIER sagen wir clangd, dass wir Snippets unterstützen!
-            })
-            vim.lsp.enable("clangd")
+			-- C/C++ Setup mit der NEUEN nativen API
+			vim.lsp.config("clangd", {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--header-insertion=never",
+					"--query-driver=/usr/bin/gcc,/usr/bin/g++", -- Fix für Arch Linux
 
-            -- Rust Setup
-            vim.lsp.config("rust_analyzer", {
-                capabilities = capabilities, -- Auch hier direkt mit übergeben
-            })
-            vim.lsp.enable("rust_analyzer")
-        end,
-    },
+					-- NEUE FLAGS FÜR MEHR GESCHWINDIGKEIT:
+					"-j=4",    -- Nutzt 4 CPU-Kerne gleichzeitig (falls du einen starken PC hast, mach "8" draus)
+					"--pch-storage=memory", -- Hält den Zwischenspeicher (Precompiled Headers) im RAM statt auf der Festplatte (sehr viel schneller!)
+					"--completion-style=detailed", -- Verbessert die Lesbarkeit der Vorschläge
+				},
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				capabilities = capabilities, -- <-- HIER sagen wir clangd, dass wir Snippets unterstützen!
+			})
+			vim.lsp.enable("clangd")
+
+			-- Rust Setup
+			vim.lsp.config("rust_analyzer", {
+				capabilities = capabilities, -- Auch hier direkt mit übergeben
+			})
+			vim.lsp.enable("rust_analyzer")
+		end,
+	},
 }
